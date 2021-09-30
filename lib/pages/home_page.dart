b5c640a0eb8773f1b52c5b15b775bf71af4e6ba4
ton4023 <ton4023@gmail.com>
2021-09-30 07:02:02 +0700

    add filter user

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final userRepository = UserRepository();
-  late List<User> users;
+  // late List<User> users;
   late List<User> filteredUsers;
   bool _isSearching = false;
   final _searchQueryController = TextEditingController();
@@ -40,16 +40,18 @@ class _HomePageState extends State<HomePage> {
       body: BlocBuilder<UserCubit, UserState>(
         builder: (context, state) {
           if (state is UserLoaded) {
-            users = state.users;
+            filteredUsers = state.users;
             List<User> _users = state.users;
             return ListView.builder(
-                itemCount: users.length,
+                itemCount: _users.length,
                 itemBuilder: (context, index) =>
                     CardWidget(state: _users[index]));
           }
           if (state is UserFiltered) {
-            print(state);
-            return Container();
+            return ListView.builder(
+                itemCount: filteredUsers.length,
+                itemBuilder: (context, index) =>
+                    CardWidget(state: filteredUsers[index]));
           }
           return Container();
         },
@@ -58,35 +60,30 @@ class _HomePageState extends State<HomePage> {
   }
 
   _buildSearchField() {
-    return BlocBuilder<UserCubit, UserState>(
-      builder: (context, state) {
-        print('_buildSearchField');
-        return TextField(
-          controller: _searchQueryController,
-          autofocus: true,
-          decoration: InputDecoration(
-            hintText: "Search Username,Firstname,Lastname...",
-            border: InputBorder.none,
-            hintStyle: TextStyle(color: Colors.white30),
-          ),
-          style: TextStyle(color: Colors.white, fontSize: 16.0),
-          onChanged: (query) {
-            setState(() {
-              filteredUsers = users
-                  .where((item) => (item.firstname
-                          .toLowerCase()
-                          .contains(query.toLowerCase()) ||
-                      item.lastname
-                          .toLowerCase()
-                          .contains(query.toLowerCase()) ||
-                      item.email.toLowerCase().contains(query.toLowerCase())))
-                  .toList();
-              context.read<UserCubit>().filterUser(filteredUsers);
-            });
-          },
-        );
-      },
-    );
+    return TextField(
+        controller: _searchQueryController,
+        autofocus: true,
+        decoration: InputDecoration(
+          hintText: "Search Username,Firstname,Lastname...",
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: Colors.white30),
+        ),
+        style: TextStyle(color: Colors.white, fontSize: 16.0),
+        onChanged: (query) {
+          setState(() {
+            filteredUsers = filteredUsers
+                .where((item) => (
+                    // item.firstname
+                    //     .toLowerCase()
+                    //     .contains(query.toLowerCase()) ||
+                    // item.lastname
+                    //     .toLowerCase()
+                    //     .contains(query.toLowerCase()) ||
+                    item.username.toLowerCase().contains(query.toLowerCase())))
+                .toList();
+            context.read<UserCubit>().filterUser(filteredUsers);
+          });
+        });
   }
 
   _buildActions() {
@@ -94,8 +91,11 @@ class _HomePageState extends State<HomePage> {
       return IconButton(
         icon: Icon(Icons.clear),
         onPressed: () {
-          _isSearching = !_isSearching;
-          _searchQueryController.text = '';
+          setState(() {
+            _searchQueryController.text = '';
+            _isSearching = !_isSearching;
+            context.read<UserCubit>().getUser();
+          });
         },
       );
     }
@@ -103,7 +103,9 @@ class _HomePageState extends State<HomePage> {
     return IconButton(
       icon: const Icon(Icons.search),
       onPressed: () {
-        _isSearching = !_isSearching;
+        setState(() {
+          _isSearching = !_isSearching;
+        });
         print('_isSearching is $_isSearching');
       },
     );
